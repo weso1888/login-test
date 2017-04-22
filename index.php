79deea4a1bd0024e9a8e23af1b97e0d1b226f371
IamWes0ne <1512977@rgu.ac.uk>
2017-04-22 19:59:47 +0100

    test2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>PHP Login Form without Session</title>
-    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="/login-test/CSS/style.css" type="text/css" />
 </head>
 
 <body>
