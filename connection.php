d92be56ee54c4a4529d12a5a14b835ba2c1f0dc8
IamWes0ne <1512977@rgu.ac.uk>
2017-04-23 16:34:50 +0100

    test2

--- connection.php
+++ connection.php
@@ -1,8 +1,9 @@
 <?php
+
 define('DB_SERVER', 'login-test1');
 define('DB_USERNAME', 'azure');
 define('DB_PASSWORD', 'password');
-define('DB_DATABASE', 'localdb');
+define('DB_DATABASE', 'login-test');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 $connectstr_dbhost = '';
